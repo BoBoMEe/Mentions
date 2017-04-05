0369bad014e2b8dc8ef52fb6a895dd86c5fd473d
BoBoMEe <wbwjx115@gamil.com>
2017-04-05 10:26:45 +0800

    提取 Util

--- mentions/src/main/java/com/bobomee/android/mentions/text/MentionTextView.java
+++ mentions/src/main/java/com/bobomee/android/mentions/text/MentionTextView.java
@@ -4,23 +4,11 @@ import android.content.Context;
 import android.graphics.Color;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
-import android.text.Spannable;
 import android.text.SpannableString;
-import android.text.TextUtils;
 import android.util.AttributeSet;
 import com.bobomee.android.mentions.listener.manager.ListenerManager;
-import com.bobomee.android.mentions.model.BaseModel;
-import com.bobomee.android.mentions.model.TagRange;
-import com.bobomee.android.mentions.model.UrlRange;
-import com.bobomee.android.mentions.model.UserRange;
 import com.bobomee.android.mentions.text.listener.SpanClickListener;
-import com.bobomee.android.mentions.text.span.AtSpan;
-import com.bobomee.android.mentions.text.span.TagSpan;
-import com.bobomee.android.mentions.text.span.UrlSpan;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import com.bobomee.android.mentions.text.util.SpanConvertUtil;
 
 /**
  * Project ID：400YF17051<br/>
@@ -33,11 +21,11 @@ import java.util.regex.Pattern;
  */
 public class MentionTextView extends TextViewFixTouchConsume {
   public MentionTextView(Context context) {
-    this(context,null);
+    this(context, null);
   }
 
   public MentionTextView(Context context, AttributeSet attrs) {
-    this(context, attrs,0);
+    this(context, attrs, 0);
   }
 
   public MentionTextView(Context context, AttributeSet attrs, int defStyleAttr) {
@@ -50,31 +38,26 @@ public class MentionTextView extends TextViewFixTouchConsume {
     super(context, attrs, defStyleAttr, defStyleRes);
     onInitialize();
   }
+
   @Override public void setText(CharSequence text, BufferType type) {
-    SpannableString weiBoText = getWeiBoText(getContext(), text.toString());
+    SpannableString weiBoText = mSpanConvertUtil.getWeiBoText(text.toString());
     setMovementMethod(TextViewFixTouchConsume.LocalLinkMovementMethod.getInstance());
     super.setText(weiBoText, type);
   }
 
   public void clear() {
     setText("");
-    mRangeSet.clear();
+    mSpanConvertUtil.clear();
   }
 
   ////////////////////////////////////////////////////////////////////////////////////////////////////
-  private static final Pattern URL_PATTERN = Pattern.compile(
-      "((http|https|ftp|ftps):\\/\\/)?([a-zA-Z0-9-]+\\.){1,5}(com|cn|net|org|hk|tw)((\\/(\\w|-)+(\\.([a-zA-Z]+))?)+)?(\\/)?(\\??([\\.%:a-zA-Z0-9_-]+=[#\\.%:a-zA-Z0-9_-]+(&amp;)?)+)?");
-  private static final Pattern AT_PATTERN =
-      Pattern.compile("(\\(@[\\u4e00-\\u9fa5\\w\\-\\$]+[-,.?:;'\"!]+[a-z]+[=\\-]+[\\w\\-\\$]+\\))");
-  private static final Pattern TAG_PATTERN = Pattern.compile("#([^\\#|.]+)#");
-
-  private Set<com.bobomee.android.mentions.model.Range> mRangeSet;
+  private SpanConvertUtil mSpanConvertUtil;
   private int mMentionTextColor;
   private int mTagTextColor;
   private int mUrlTextColor;
 
   private void onInitialize() {
-    mRangeSet = new HashSet<>();
+    mSpanConvertUtil = new SpanConvertUtil(this);
     mMentionTextColor = Color.RED;
     mTagTextColor = Color.BLUE;
     mUrlTextColor = Color.GRAY;
@@ -82,10 +65,6 @@ public class MentionTextView extends TextViewFixTouchConsume {
 
   public int getUrlTextColor() {
     return mUrlTextColor;
-  }
-
-  public void setUrlTextColor(int urlTextColor) {
-    mUrlTextColor = urlTextColor;
   }
 
   public int getMentionTextColor() {
@@ -96,6 +75,10 @@ public class MentionTextView extends TextViewFixTouchConsume {
     return mTagTextColor;
   }
 
+  public void setUrlTextColor(int urlTextColor) {
+    mUrlTextColor = urlTextColor;
+  }
+
   public void setMentionTextColor(int mentionTextColor) {
     mMentionTextColor = mentionTextColor;
   }
@@ -104,74 +87,9 @@ public class MentionTextView extends TextViewFixTouchConsume {
     mTagTextColor = tagTextColor;
   }
 
-  private void addRange(com.bobomee.android.mentions.model.Range range) {
-    String lable = range.getLable();
-    String id = range.getId();
-    if (!TextUtils.isEmpty(lable) && !TextUtils.isEmpty(id)) {
-      mRangeSet.add(range);
-    }
-  }
-
-  private SpannableString getWeiBoText(Context context, String text) {
-
-    if (TextUtils.isEmpty(text)) {
-      return new SpannableString("");
-    }
-
-    // 1, 处理@昵称
-    Matcher at = AT_PATTERN.matcher(text);
-    while (at.find()) {
-      String key = at.group(0);
-
-      String name = "", id = "";
-
-      int indexOf = key.indexOf(",id=");
-      if (indexOf > 0) {
-        name = key.substring(1, indexOf);
-        id = key.substring(indexOf + 4, key.length() - 1);
-      }
-
-      text = text.replace(key, name);
-
-      int start = text.indexOf(name);
-
-      com.bobomee.android.mentions.model.Range range = new UserRange(id, name, start, start + name.length());
-      addRange(range);
-    }
-
-    SpannableString spannable = new SpannableString(text);
-
-    for (com.bobomee.android.mentions.model.Range range : mRangeSet) {
-      if (range.getType() == BaseModel.TYPE_USER) {
-        spannable.setSpan(new AtSpan(this, range), range.getFrom(), range.getTo(),
-            Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
-      }
-    }
-
-    Matcher tag = TAG_PATTERN.matcher(spannable);
-    while (tag.find()) {
-      String tagNameMatch = tag.group();
-      int start = tag.start();
-      com.bobomee.android.mentions.model.Range range = new TagRange(tagNameMatch, start, start + tagNameMatch.length());
-      spannable.setSpan(new TagSpan(this, range), start, start + tagNameMatch.length(),
-          Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
-    }
-
-    Matcher url = URL_PATTERN.matcher(spannable);
-    while (url.find()) {
-      String urlString = url.group();
-      int start = url.start();
-      com.bobomee.android.mentions.model.Range range = new UrlRange(urlString, start, start + urlString.length());
-      spannable.setSpan(new UrlSpan(this, range), start, start + urlString.length(),
-          Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
-    }
-
-    return spannable;
-  }
-
   private ListenerManager mListenerManager = ListenerManager.INSTANCE;
-  public void  addSpanClickListener(SpanClickListener spanClickListener){
+
+  public void addSpanClickListener(SpanClickListener spanClickListener) {
     mListenerManager.addSpanClickListener(spanClickListener);
   }
-
 }
---
+++ mentions/src/main/java/com/bobomee/android/mentions/text/util/SpanConvertUtil.java
@@ -0,0 +1,117 @@
+package com.bobomee.android.mentions.text.util;
+
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.TextUtils;
+import com.bobomee.android.mentions.model.BaseModel;
+import com.bobomee.android.mentions.model.Range;
+import com.bobomee.android.mentions.model.TagRange;
+import com.bobomee.android.mentions.model.UrlRange;
+import com.bobomee.android.mentions.model.UserRange;
+import com.bobomee.android.mentions.text.MentionTextView;
+import com.bobomee.android.mentions.text.span.AtSpan;
+import com.bobomee.android.mentions.text.span.TagSpan;
+import com.bobomee.android.mentions.text.span.UrlSpan;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Project ID：400YF17051<br/>
+ * Resume:
+ *
+ * @author 汪波
+ * @version 1.0
+ * @see
+ * @since 2017/4/5 汪波 first commit
+ */
+public class SpanConvertUtil {
+
+  private static final Pattern URL_PATTERN = Pattern.compile(
+      "((http|https|ftp|ftps):\\/\\/)?([a-zA-Z0-9-]+\\.){1,5}(com|cn|net|org|hk|tw)((\\/(\\w|-)+(\\.([a-zA-Z]+))?)+)?(\\/)?(\\??([\\.%:a-zA-Z0-9_-]+=[#\\.%:a-zA-Z0-9_-]+(&amp;)?)+)?");
+  private static final Pattern AT_PATTERN =
+      Pattern.compile("(\\(@[\\u4e00-\\u9fa5\\w\\-\\$]+[-,.?:;'\"!]+[a-z]+[=\\-]+[\\w\\-\\$]+\\))");
+  private static final Pattern TAG_PATTERN = Pattern.compile("#([^\\#|.]+)#");
+
+  private Set<Range> mRangeSet;
+  private MentionTextView mMentionTextView;
+
+  public SpanConvertUtil(MentionTextView mentionTextView) {
+    mRangeSet = new HashSet<>();
+    mMentionTextView = mentionTextView;
+  }
+
+  private void addRange(com.bobomee.android.mentions.model.Range range) {
+    String lable = range.getLable();
+    String id = range.getId();
+    if (!TextUtils.isEmpty(lable) && !TextUtils.isEmpty(id)) {
+      mRangeSet.add(range);
+    }
+  }
+
+  public SpannableString getWeiBoText(String text) {
+
+    if (TextUtils.isEmpty(text)) {
+      return new SpannableString("");
+    }
+
+    // 1, 处理@昵称
+    Matcher at = AT_PATTERN.matcher(text);
+    while (at.find()) {
+      String key = at.group(0);
+
+      String name = "", id = "";
+
+      int indexOf = key.indexOf(",id=");
+      if (indexOf > 0) {
+        name = key.substring(1, indexOf);
+        id = key.substring(indexOf + 4, key.length() - 1);
+      }
+
+      text = text.replace(key, name);
+
+      int start = text.indexOf(name);
+
+      com.bobomee.android.mentions.model.Range range =
+          new UserRange(id, name, start, start + name.length());
+      addRange(range);
+    }
+
+    SpannableString spannable = new SpannableString(text);
+
+    for (com.bobomee.android.mentions.model.Range range : mRangeSet) {
+      if (range.getType() == BaseModel.TYPE_USER) {
+        spannable.setSpan(new AtSpan(mMentionTextView, range), range.getFrom(), range.getTo(),
+            Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
+      }
+    }
+
+    Matcher tag = TAG_PATTERN.matcher(spannable);
+    while (tag.find()) {
+      String tagNameMatch = tag.group();
+      int start = tag.start();
+      com.bobomee.android.mentions.model.Range range =
+          new TagRange(tagNameMatch, start, start + tagNameMatch.length());
+      spannable.setSpan(new TagSpan(mMentionTextView, range), start, start + tagNameMatch.length(),
+          Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
+    }
+
+    Matcher url = URL_PATTERN.matcher(spannable);
+    while (url.find()) {
+      String urlString = url.group();
+      int start = url.start();
+      com.bobomee.android.mentions.model.Range range =
+          new UrlRange(urlString, start, start + urlString.length());
+      spannable.setSpan(new UrlSpan(mMentionTextView, range), start, start + urlString.length(),
+          Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
+    }
+
+    return spannable;
+  }
+
+  public void clear() {
+    mRangeSet.clear();
+  }
+
+}
